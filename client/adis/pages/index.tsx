5519908707a6c3a10b1c95f70352a6498d9a9dec
adis-tek <67308354+adis-tek@users.noreply.github.com>
2022-04-17 20:11:26 -0500

    added the hero flex

--- client/adis/pages/index.tsx
+++ client/adis/pages/index.tsx
@@ -5,9 +5,52 @@ import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <>
-    <h1>Adis has spun up.</h1>
-    </>
+    <div className={styles.container}>
+      <div className={styles.hero}>
+        <div className={styles.menuContainer}>
+        <h1 className={styles.logo}>Adis</h1>
+        <div className={styles.iconWrapper}>
+        <Image
+          src="/adis-menu.png"
+          alt="menu icon"
+          width={50}
+          height={34}
+          className={styles.menuIcon}
+        />
+        </div>
+        </div>
+      </div>
+      <h1>Adis has spun up.</h1>
+      <div className={styles.featuredPhotosContainer}>
+        <Image
+          src="/featured-1.jpg"
+          alt="screenshot of an app"
+          width={180}
+          height={235}
+          className={styles.featuredPhoto}
+        />
+        <Image
+          src="/featured-2.jpg"
+          alt="screenshot of an app"
+          width={180}
+          height={235}
+          className={styles.featuredPhoto}
+        />
+        </div>
+        <div className={styles.featuredProjectsContainer}>
+          <div className={styles.leftProject}>
+            <h1 className={styles.number}>01</h1>
+            <h1 className={styles.title}>Audiophile</h1>
+            <Image
+              src="/featured-2.jpg"
+              alt="screenshot of an app"
+              width={180}
+              height={235}
+              className={styles.featuredPhoto}
+            />
+          </div>
+        </div>
+    </div>
   )
 }
 
