a2245458263173bd1a561ebd47be460594e65586
adis-tek <67308354+adis-tek@users.noreply.github.com>
2022-04-19 01:53:19 -0500

    page templates created

--- client/adis/components/aboutPage/aboutPageTemplate.tsx
+++ client/adis/components/aboutPage/aboutPageTemplate.tsx
@@ -2,13 +2,56 @@ import type { NextPage } from 'next'
 import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import styles from '../../styles/aboutPageTemplate.module.scss'
 
 const About: NextPage = (props) => {
+    const [faq, setFaq] = useState(false);
+
+    function toggleFaq () {
+      setFaq(!faq);
+    }
 
   return (
     <div className={styles.container}>
-        <p>About Page</p>
+        <div className={styles.introContainer}>
+            <h1 className={styles.heading}>Enjoying the Process</h1>
+            <p className={styles.excerpt}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
+            <div className={styles.buttonContainer} role="button">
+            <h2 className={styles.text}>See Github</h2>
+            <div className={styles.imageWrapper}>
+                <Image
+                src="/button-arrow.png"
+                alt="arrow pointing up to the right corner of the screen"
+                width={17}
+                height={17}
+                className={styles.buttonArrow}
+                />
+            </div>
+            </div>
+            <div className={styles.outlinedShadow} />
+      </div>
+      <div className={styles.excerptContainer}>
+          <h1 className={styles.heading}>Enjoying the Process</h1>
+          <p className={styles.excerpt}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
+      </div>
+    <div className={styles.faqContainer}>
+        <div className={styles.faqBox} onClick={toggleFaq}>
+            <div className={styles.qaContainer}>
+            <div className={styles.questionContainer}>
+            <h2 className={styles.question}>01. Meaning of life?</h2>
+            {faq === true &&
+            <h1 className={styles.close}>X</h1>
+            }
+            {faq === false &&
+            <h1 className={styles.open}>O</h1>
+            }
+            </div>
+            {faq && 
+                <p className={styles.answer}>The answer is 42.</p>
+            }
+            </div>
+        </div>
+    </div>
     </div>
   )
 }
--- client/adis/components/projectPage/projectPageTemplate.tsx
+++ client/adis/components/projectPage/projectPageTemplate.tsx
@@ -2,20 +2,78 @@ import type { NextPage } from 'next'
 import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-// import styles from '../styles/Home.module.scss'
+import styles from '../../styles/projectPageTemplate.module.scss'
 
 const Projects: NextPage = (props) => {
 
   return (
-    <div>
-        <p>Projects Page</p>
-        <p>{props.name}</p>
-        <Image
-            src={props.photo}
-            alt="menu icon"
-            width={250}
-            height={250}
-            />
+    <div className={styles.container}>
+        <div className={styles.introContainer}>
+            <h1 className={styles.heading}>Enjoying the Process</h1>
+            <p className={styles.excerpt}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
+            <div className={styles.buttonContainer} role="button">
+            <h2 className={styles.text}>Visit Site</h2>
+            <div className={styles.imageWrapper}>
+                <Image
+                src="/button-arrow.png"
+                alt="arrow pointing up to the right corner of the screen"
+                width={17}
+                height={17}
+                className={styles.buttonArrow}
+                />
+            </div>
+            </div>
+            <div className={styles.outlinedShadow} />
+            <div className={styles.buttonContainer} role="button">
+            <h2 className={styles.text}>View Code</h2>
+            <div className={styles.imageWrapper}>
+                <Image
+                src="/button-arrow.png"
+                alt="arrow pointing up to the right corner of the screen"
+                width={17}
+                height={17}
+                className={styles.buttonArrow}
+                />
+            </div>
+            </div>
+            <div className={styles.outlinedShadow} />
+      </div>
+      <div className={styles.slideshowContainer}>
+          <p>Slideshow</p>
+      </div>
+      <div className={styles.excerptContainer}>
+          <h1 className={styles.heading}>Enjoying the Process</h1>
+          <p className={styles.excerpt}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
+    </div>
+    <div className={styles.screenshotsContainer}>
+        <div className={styles.imageWrapper}>
+            <Image
+                src="/featured-2.jpg"
+                alt="a visual app demo"
+                width={250}
+                height={250}
+                className={styles.screenshot}
+                />
+        </div>
+        <div className={styles.imageWrapper}>
+            <Image
+                src="/featured-2.jpg"
+                alt="a visual app demo"
+                width={250}
+                height={250}
+                className={styles.screenshot}
+                />
+        </div>
+        <div className={styles.imageWrapper}>
+            <Image
+                src="/featured-2.jpg"
+                alt="a visual app demo"
+                width={250}
+                height={250}
+                className={styles.screenshot}
+                />
+        </div>
+    </div>
     </div>
   )
 }
