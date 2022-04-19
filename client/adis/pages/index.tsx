b8d78fd294fbe45e63c6dbb8768b57428f346212
adis-tek <67308354+adis-tek@users.noreply.github.com>
2022-04-18 20:18:50 -0500

    mobile menu created

--- client/adis/pages/index.tsx
+++ client/adis/pages/index.tsx
@@ -1,11 +1,38 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
+  const [menu, setMenu] = useState(false);
+
+  function toggleMenu () {
+    setMenu(!menu);
+  }
+
   return (
     <div className={styles.container}>
+      {menu && 
+        <div className={styles.toggleMenuContainer}>
+          <div className={styles.topBannerContainer}>
+          <h2 className={styles.subheader}>Pages</h2>
+          <h3 className={styles.menuClose} onClick={toggleMenu}>X</h3>
+          </div>
+          <div className={styles.pageTitleContainer}>
+          <h1 className={styles.pageTitle}>Projects</h1>
+          <h6 className={styles.exponent}>01</h6>
+          </div>
+          <div className={styles.pageTitleContainer}>
+          <h1 className={styles.pageTitle}>Resume</h1>
+          <h6 className={styles.exponent}>02</h6>
+          </div>
+          <div className={styles.pageTitleContainer}>
+          <h1 className={styles.pageTitle}>About</h1>
+          <h6 className={styles.exponent}>03</h6>
+          </div>
+        </div>
+      }
       <div className={styles.hero}>
         <div className={styles.menuContainer}>
         <h1 className={styles.logo}>Adis</h1>
@@ -15,6 +42,7 @@ const Home: NextPage = () => {
           alt="menu icon"
           width={50}
           height={34}
+          onClick={toggleMenu}
           className={styles.menuIcon}
         />
         </div>
